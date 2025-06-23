4757194a90f4626886debd1b8ca7e81a7e32539d
FousiyaN <fousiyafousy01@gmail.com>
2025-06-23 16:52:07 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function clickme(val)
+{
+    
+    document.getElementById("screen").value+=val
+}
+
+function clearscreen(){
+    document.getElementById("screen").value=""
+}
+
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
